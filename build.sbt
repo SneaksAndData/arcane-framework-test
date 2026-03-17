4f05b05e9f608715421e0778fbf68c1c6c932e9e
George Zubrienko <gzu@ecco.com>
2026-03-17 18:05:08 +0100

    Adapt to scoped layers

--- build.sbt
+++ build.sbt
@@ -39,7 +39,7 @@ lazy val root = (project in file("."))
       name := "arcane-framework-test",
       idePackagePrefix := Some("com.sneaksanddata.arcane.framework.testkit"),
 
-      libraryDependencies += "com.sneaksanddata" % "arcane-framework_3" % "2.1.1-48-gd9b55d1",
+      libraryDependencies += "com.sneaksanddata" % "arcane-framework_3" % "2.1.1-56-ged1473b",
 
       // Compiler options
       Test / logBuffered := false,
--- src/main/scala/setups/FrameworkTestSetup.scala
+++ src/main/scala/setups/FrameworkTestSetup.scala
@@ -5,15 +5,17 @@ import iceberg.{TestEntityManager, TestPropertyManager}
 
 import com.sneaksanddata.arcane.framework.models.ddl.CreateTableRequest
 import com.sneaksanddata.arcane.framework.models.schemas.ArcaneSchema
+import com.sneaksanddata.arcane.framework.services.iceberg.base.{SinkEntityManager, SinkPropertyManager}
 import com.sneaksanddata.arcane.framework.services.iceberg.given_Conversion_ArcaneSchema_Schema
 import com.sneaksanddata.arcane.framework.services.streaming.base.JsonWatermark
-import zio.Task
+import zio.{Task, ZIO}
 
 object FrameworkTestSetup:
-  def prepareWatermark(tableName: String, schema: ArcaneSchema, watermark: JsonWatermark): Task[Unit] =
-    val propertyManager = TestPropertyManager.sinkPropertyManager()
-    val entityManager   = TestEntityManager.sinkEntityManager()
-    for
-      _ <- entityManager.createTable(CreateTableRequest(tableName, schema, true))
-      _ <- propertyManager.comment(tableName, watermark.toJson)
-    yield ()
+  def prepareWatermark(tableName: String, schema: ArcaneSchema, watermark: JsonWatermark): Task[Unit] = ZIO
+    .scoped {
+      for
+        _ <- ZIO.service[SinkEntityManager].map(_.createTable(CreateTableRequest(tableName, schema, true)))
+        _ <- ZIO.service[SinkPropertyManager].map(_.comment(tableName, watermark.toJson))
+      yield ()
+    }
+    .provide(TestPropertyManager.sinkPropertyManagerLayer, TestEntityManager.sinkEntityManagerLayer)
